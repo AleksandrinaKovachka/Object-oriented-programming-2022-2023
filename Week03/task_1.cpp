770bbaba101a65c06d707e1802b5f097f82c62c3
Aleksandrina Kovachka <56196244+AleksandrinaKovachka@users.noreply.github.com>
2023-03-22 22:08:19 +0200

    Add tasks for week 3-4

---
+++ Week03/task_1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+void createFile(const char* fileName, std::string text)
+{
+    std::ofstream out(fileName);
+    if(out.is_open())
+    {
+        out << text << '\n';
+        out.close();
+    }
+
+}
+
+std::string firstLetters(const char* fileName)
+{
+    std::ifstream in(fileName);
+    std::string line;
+    std::string result;
+    if(in.is_open())
+    {
+        while(std::getline(in , line))
+        {
+            result += line[0];
+        }
+
+        in.close();
+    }
+    return result;
+}
+
+void printFile(const char* fileName)
+{
+    std::ifstream in(fileName);
+    if(in.is_open())
+    {
+        std::string line;
+        while(std::getline(in , line))
+        {
+            std::cout<<line<<std::endl;
+        }
+        in.close();
+    }
+}
+
+int main()
+{
+    
+    return 0;
+}
